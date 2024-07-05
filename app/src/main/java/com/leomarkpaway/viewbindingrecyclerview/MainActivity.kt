0f5caead9aa398374848cc4129a4a52bb992a907
leomarkpaway <leomarkpaway@gmail.com>
2024-07-04 19:18:17 -0700

    :sparkles: Display all mock item

--- app/src/main/java/com/leomarkpaway/viewbindingrecyclerview/MainActivity.kt
+++ app/src/main/java/com/leomarkpaway/viewbindingrecyclerview/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.leomarkpaway.viewbindingrecyclerview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.leomarkpaway.viewbindingrecyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +12,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val mockItem: ArrayList<Item> = ArrayList()
+        for (number in 1..10) {
+            mockItem.add(Item("Item $number"))
+        }
+
+        setupItemList(mockItem)
+
     }
+
+    private fun setupItemList(items: List<Item>) = with(binding.rvItemList) {
+        adapter = ItemAdapter(items)
+        layoutManager = LinearLayoutManager(this@MainActivity, LinearLayoutManager.VERTICAL, false)
+    }
+
 }
