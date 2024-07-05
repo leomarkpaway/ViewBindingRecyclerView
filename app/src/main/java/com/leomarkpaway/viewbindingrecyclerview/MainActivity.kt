090eab1bf1ba58ba426bddd30805dee73f3cfda7
leomarkpaway <leomarkpaway@gmail.com>
2024-07-04 18:44:46 -0700

    :sparkles: Implement view binding main activity

--- app/src/main/java/com/leomarkpaway/viewbindingrecyclerview/MainActivity.kt
+++ app/src/main/java/com/leomarkpaway/viewbindingrecyclerview/MainActivity.kt
@@ -2,10 +2,15 @@ package com.leomarkpaway.viewbindingrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.leomarkpaway.viewbindingrecyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
